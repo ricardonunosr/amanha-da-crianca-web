249b992d06743cb88c4ab831b0d1746d30abc1bc
Ricardo Ribeiro <ricardonunosr@gmail.com>
2020-11-04 17:40:04 +0000

    Added dotenv for deploy

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyBEYwBBxPQLlZL2eI26DaHNlV2YuZj_h2I',
-  authDomain: 'amanha-da-crianca.firebaseapp.com',
-  databaseURL: 'https://amanha-da-crianca.firebaseio.com',
-  projectId: 'amanha-da-crianca',
-  storageBucket: 'amanha-da-crianca.appspot.com',
-  messagingSenderId: '604731762346',
-  appId: '1:604731762346:web:278e9e37c9110c876512d3',
-  measurementId: 'G-R54KDE4CLD'
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export const auth = firebase.auth();
